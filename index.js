adebc6bedef1bbe5ff03556bce8b2a69a4178051
nelmaf <nelmaf@me.com>
2023-03-21 20:54:33 -0400

    added the for loop

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-window.prompt(weeklyExpensesQuestions[0])
-let answer = window.prompt(weeklyExpensesQuestions[0])
-console.log(answer)
 
-let stringAnswer = window.prompt(weeklyExpensesQuestions[0])
-let numberAnswer = parseFloat(stringAnswer)
-console.log(numberAnswer)
+for(let i = 0; i < weeklyExpenseQuestions.length; ){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
 
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-console.log(answer)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
