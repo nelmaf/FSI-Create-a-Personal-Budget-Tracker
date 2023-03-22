2356e56b0dcd99f2d72348b1b1b07e387db0d05b
nelmaf <nelmaf@me.com>
2023-03-21 20:47:54 -0400

    Add to js file

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+window.prompt(weeklyExpensesQuestions[0])
+let answer = window.prompt(weeklyExpensesQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpensesQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
